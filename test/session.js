a9cb03809ae6378933079ca6470903beb9da4e98
vogdb <vozhdb@gmail.com>
2016-03-17 12:49:21 +0300

    Use Connection stub in Session tests

--- test/session.js
+++ test/session.js
@@ -9,11 +9,16 @@ var Plugin = require('../src/plugin');
 
 describe('Session tests', function() {
 
+  var connection;
+  beforeEach(function() {
+    connection = sinon.createStubInstance(Connection);
+    connection.getOptions.returns({});
+  });
+
   context('basic operations', function() {
-    var connection, session;
+    var session;
 
     beforeEach(function() {
-      connection = new Connection('connection-id', {address: ''});
       session = new Session(connection, 'id');
     });
 
@@ -23,12 +28,14 @@ describe('Session tests', function() {
     });
 
     it('is destroyed on connection.close', function(done) {
+      var connection = new Connection('', '');
+      session = new Session(connection, 'id');
       sinon.spy(session, '_destroy');
       session.on('destroy', function() {
         assert.isTrue(session._destroy.calledOnce);
         done();
       });
-      connection.close();
+      connection.emit('close');
     });
 
     it('adds transaction', function(done) {
@@ -42,7 +49,6 @@ describe('Session tests', function() {
 
     it('sends message with session_id', function() {
       var message;
-      sinon.stub(session._connection, 'send');
 
       message = {};
       session.send(message);
@@ -75,13 +81,12 @@ describe('Session tests', function() {
   });
 
   context('keepalive works', function() {
-    var connection, session, keepAlivePeriod = 500;
+    var session, keepAlivePeriod = 500;
 
     beforeEach(function() {
-      connection = new Connection('connection-id', {address: '', keepalive: keepAlivePeriod});
+      connection.getOptions.returns({keepalive: keepAlivePeriod});
       session = new Session(connection, 'id');
       sinon.spy(session, 'send');
-      sinon.stub(session._connection, 'sendSync');
     });
 
     it('is sent periodically when session is inactive', function(done) {
@@ -123,7 +128,7 @@ describe('Session tests', function() {
     var session, plugin;
 
     beforeEach(function() {
-      session = new Session(new Connection('id', {address: ''}), 'id');
+      session = new Session(connection, 'id');
       plugin = new Plugin(session, 'name', 'id');
     });
 
@@ -147,7 +152,7 @@ describe('Session tests', function() {
     var session;
 
     beforeEach(function() {
-      session = new Session(new Connection('id', {address: ''}), 'id');
+      session = new Session(connection, 'id');
     });
 
     it('calls _onTimeout for timeout message', function() {
@@ -200,7 +205,7 @@ describe('Session tests', function() {
     var session;
 
     beforeEach(function() {
-      session = new Session(new Connection('id', {address: ''}), 'id');
+      session = new Session(connection, 'id');
     });
 
     it('calls _onAttach for attach message', function() {
@@ -261,7 +266,7 @@ describe('Session tests', function() {
     var session;
 
     beforeEach(function() {
-      session = new Session(new Connection, 'id');
+      session = new Session(connection, 'id');
       sinon.stub(session, 'send');
       sinon.stub(session, 'addTransaction');
     });
