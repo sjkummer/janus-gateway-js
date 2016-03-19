e817ae11dcd7ceb8bce93d86cec21ea42c8218da
vogdb <vozhdb@gmail.com>
2016-03-19 12:43:32 +0300

    Merge browserify gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,15 +7,19 @@ var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
+var exorcist = require('exorcist');
 
-gulp.task('browserify-min', function () {
+gulp.task('browserify', function() {
   var b = browserify({
     entries: './src/index.js',
     debug: true
   });
 
   return b.bundle()
-    .pipe(source('janus.min.js'))
+    .pipe(exorcist('./dist/janus.js.map'))
+    .pipe(source('janus.js'))
+    .pipe(gulp.dest('./dist'))
+    .pipe(rename('janus.min.js'))
     .pipe(buffer())
     .pipe(sourcemaps.init({loadMaps: true}))
     .pipe(uglify())
@@ -24,21 +28,10 @@ gulp.task('browserify-min', function () {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('browserify', function() {
-  var b = browserify({
-    entries: './src/index.js'
-  });
-
-  return b.bundle()
-    .pipe(source('janus.js'))
-    .pipe(gulp.dest('./dist'));
-});
-
-
 gulp.task('default', function() {
   gulp.run('browserify');
 
   gulp.watch("./src/*.js", function() {
-    gulp.run(['browserify', 'browserify-min']);
+    gulp.run('browserify');
   });
 });
