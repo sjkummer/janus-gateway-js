98c56fcf10f8694e9e407dba1e570ba57c6c982f
vogdb <vozhdb@gmail.com>
2016-05-18 14:09:41 +0300

    Export Transaction

--- src/browser.js
+++ src/browser.js
@@ -1,6 +1,7 @@
 var Client = require('./client');
 var JanusError = require('./error');
 var WebsocketConnection = require('./websocket-connection');
+var Transaction = require('./transaction');
 var Connection = require('./connection');
 var Session = require('./session');
 var Plugin = require('./plugin');
@@ -13,5 +14,6 @@ window.Janus = {
   Session: Session,
   Plugin: Plugin,
   MediaPlugin: MediaPlugin,
-  WebsocketConnection: WebsocketConnection
+  WebsocketConnection: WebsocketConnection,
+  Transaction: Transaction
 };
