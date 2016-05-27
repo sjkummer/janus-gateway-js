026f87abb122627b758ccb650375567f3a04d620
vogdb <vozhdb@gmail.com>
2016-05-27 17:45:48 +0100

    MediaPlugin

---
+++ src/webrtc/media-devices-shim.js
@@ -0,0 +1,135 @@
+var Promise = require('bluebird');
+
+function MediaDevicesShim() {
+}
+
+MediaDevicesShim.getUserMedia = function(constraints) {
+  if (constraints.video === 'screen') {
+    return this.getSharedScreen({audio: constraints.audio});
+  } else {
+    return Promise.resolve(navigator.mediaDevices.getUserMedia(constraints));
+  }
+};
+
+MediaDevicesShim.getSharedScreen = function(constraints) {
+  if (window.location.protocol !== 'https:') {
+    return Promise.reject(new Error('Screen sharing only works on HTTPS, try the https:// version of this page'));
+  }
+
+  if (window.navigator.userAgent.match('Chrome')) {
+    return this._getSharedScreenChrome(constraints);
+  } else if (window.navigator.userAgent.match('Firefox')) {
+    return this._getSharedScreenFirefox(constraints);
+  }
+};
+
+MediaDevicesShim._getSharedScreenChrome = function(constraints) {
+  var chromever = parseInt(window.navigator.userAgent.match(/Chrome\/(.*) /)[1], 10);
+  var maxver = 33;
+  if (window.navigator.userAgent.match('Linux')) {
+    maxver = 35;
+  }	// "known" crash in chrome 34 and 35 on linux
+  if (chromever >= 26 && chromever <= maxver) {
+    // Chrome 26->33 requires some awkward chrome://flags manipulation
+    constraints = Helpers.extend({}, constraints, {
+      video: {
+        mandatory: {
+          googLeakyBucket: true,
+          maxWidth: window.screen.width,
+          maxHeight: window.screen.height,
+          maxFrameRate: 3,
+          chromeMediaSource: 'screen'
+        }
+      }
+    });
+    return window.getUserMedia(constraints);
+  } else {
+    // Chrome 34+ requires an extension
+    return new Promise(function(resolve, reject) {
+      var timeoutRejectId;
+      var messageListener = function(event) {
+        if (event.origin != window.location.origin) {
+          return;
+        }
+        if (event.data.type == 'janusGotScreen' && timeoutRejectId == event.data.id) {
+          window.clearTimeout(timeoutRejectId);
+          window.removeEventListener('message', messageListener);
+
+          if (event.data.sourceId === '') {
+            // user canceled
+            var error = new Error('NavigatorUserMediaError');
+            error.name = 'You cancelled the request for permission, giving up...';
+            reject(error);
+          } else {
+            constraints = Helpers.extend({}, constraints, {
+              video: {
+                mandatory: {
+                  chromeMediaSource: 'desktop',
+                  maxWidth: window.screen.width,
+                  maxHeight: window.screen.height,
+                  maxFrameRate: 3
+                },
+                optional: [
+                  {googLeakyBucket: true},
+                  {googTemporalLayeredScreencast: true}
+                ]
+              }
+            });
+            constraints.video.mandatory.chromeMediaSourceId = event.data.sourceId;
+            resolve(window.getUserMedia(constraints));
+          }
+        } else if (event.data.type == 'janusGetScreenPending') {
+          if (timeoutRejectId == event.data.id) {
+            window.clearTimeout(timeoutRejectId);
+          }
+        }
+      };
+
+      window.addEventListener('message', messageListener);
+      timeoutRejectId = setTimeout(
+        function() {
+          window.removeEventListener('message', messageListener);
+          var error = new Error('NavigatorUserMediaError');
+          error.name = 'The required Chrome extension is not installed: click <a href="#">here</a> to install it. (NOTE: this will need you to refresh the page)';
+          reject(error);
+        }, 1000);
+      window.postMessage({type: 'janusGetScreen', id: timeoutRejectId}, '*');
+    });
+  }
+};
+
+MediaDevicesShim._getSharedScreenFirefox = function(constraints) {
+  //TODO use UserAgent lib for version/OS/browser detection
+  var ffver = parseInt(window.navigator.userAgent.match(/Firefox\/(.*)/)[1], 10);
+  if (ffver >= 33) {
+    constraints = Helpers.extend({}, constraints, {
+      video: {
+        mozMediaSource: 'window',
+        mediaSource: 'window'
+      }
+    });
+    return window.getUserMedia(constraints).then(function(stream) {
+      if (stream) {
+        // Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=1045810
+        var lastStreamTime = stream.currentTime;
+        var inderval = window.setInterval(function() {
+          if (stream.currentTime == lastStreamTime) {
+            window.clearInterval(inderval);
+            if (stream.onended) {
+              stream.onended();
+            }
+          }
+          lastStreamTime = stream.currentTime;
+        }, 500);
+      }
+      return stream;
+    });
+  } else {
+    //var error = new Error('NavigatorUserMediaError');
+    //error.name = 'Your version of Firefox does not support screen sharing, please install Firefox 33 (or more recent versions)';
+    var error = new Error('Your version of Firefox does not support screen sharing, please install Firefox 33 (or more recent versions)');
+    return Promise.reject(error);
+  }
+};
+
+module.exports = MediaDevicesShim;
---
+++ src/webrtc/media-plugin.js
@@ -0,0 +1,161 @@
+var Promise = require('bluebird');
+var webrtcsupport = require('webrtcsupport');
+var Helpers = require('../helpers');
+var Plugin = require('../plugin');
+var MediaDevicesShim = require('./media-devices-shim');
+var IceCandidateListener = require('./ice-candidate-listener');
+
+/**
+ * @inheritDoc
+ *
+ * @constructor
+ * @extends Plugin
+ */
+function MediaPlugin(session, name, id) {
+  if (!webrtcsupport.support) {
+    throw new Error('WebRTC is not supported');
+  }
+  MediaPlugin.super_.apply(this, arguments);
+}
+
+Helpers.inherits(MediaPlugin, Plugin);
+
+/**
+ * @param {Object} [options]
+ * @returns {PeerConnection}
+ */
+MediaPlugin.prototype.createPeerConnection = function(options) {
+  options = Helpers.extend(options || {}, this._session._connection._options.pc);
+
+  var config = {
+    iceServers: [{url: 'stun:stun.l.google.com:19302'}]
+  };
+  var constraints = {
+    'optional': [{'DtlsSrtpKeyAgreement': true}]
+  };
+  if (options.config) {
+    Helpers.extend(config, options.config);
+  }
+  if (options.constraints) {
+    Helpers.extend(constraints, options.constraints);
+  }
+
+  this._pc = new webrtcsupport.PeerConnection(config, constraints);
+
+  this._pc.onaddstream = function(event) {
+    this.emit('addstream', event);
+  }.bind(this);
+
+  return this._pc;
+};
+
+/**
+ * @param stream
+ */
+MediaPlugin.prototype.addStream = function(stream) {
+  this._pc.addStream(stream);
+};
+
+/**
+ * According to https://w3c.github.io/mediacapture-main/getusermedia.html#media-track-constraints there are no constraints as `offerToReceiveVideo`
+ *
+ * @param {Object} constraints https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia#Parameters
+ * @return {Promise}
+ */
+MediaPlugin.prototype.getLocalMedia = function(constraints) {
+  this.emit('consent-dialog:start');
+  var self = this;
+  var promise = MediaDevicesShim.getUserMedia(constraints);
+  return promise
+    .then(function(stream) {
+      self.emit('consent-dialog:stop', {stream: stream});
+      return stream;
+    })
+    .catch(function(error) {
+      self.emit('consent-dialog:stop', {error: error});
+      throw error;
+    })
+    .finally(function() {
+      if (promise.isCancelled()) {
+        self.emit('consent-dialog:stop');
+      }
+    });
+};
+
+/**
+ * @param {Object} [options] @see https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createOffer#RTCOfferOptions_dictionary
+ */
+MediaPlugin.prototype.createOffer = function(options) {
+  return this._createSDP('createOffer', options);
+};
+
+/**
+ * @param {SessionDescription} jsep
+ * @param {Object} [options] @see https://developer.mozilla.org/en-US/docs/Web/API/RTCPeerConnection/createAnswer#RTCAnswerOptions_dictionary
+ */
+MediaPlugin.prototype.createAnswer = function(jsep, options) {
+  var self = this;
+  return Promise.try(function() {
+    return self.setRemoteSDP(jsep);
+  }).then(function() {
+    return self._createSDP('createAnswer', options);
+  });
+};
+
+MediaPlugin.prototype.setRemoteSDP = function(jsep) {
+  return this._pc.setRemoteDescription(new webrtcsupport.SessionDescription(jsep));
+};
+
+/**
+ * @param {string} party
+ * @param {Object} [options]
+ */
+MediaPlugin.prototype._createSDP = function(party, options) {
+  if (!this._pc) {
+    throw new Error('Create PeerConnection before creating SDP for it');
+  }
+  if (['createOffer', 'createAnswer'].indexOf(party) < 0) {
+    throw new Error('Unknown party in _createSDP');
+  }
+
+  options = options || {};
+  this._iceListener = new IceCandidateListener(this._pc);
+  var self = this;
+
+  this._iceListener.on('candidate', function(candidate) {
+    self.send({janus: 'trickle', candidate: candidate.toJSON()});
+  });
+  this._iceListener.on('complete', function() {
+    self.send({janus: 'trickle', candidate: {completed: true}});
+  });
+
+  return this._pc[party](options)
+    .then(function(description) {
+      return self._pc.setLocalDescription(description);
+    })
+    .then(function() {
+      return self._pc.localDescription;
+    });
+};
+
+MediaPlugin.prototype.processIncomeMessage = function(message) {
+  var self = this;
+  return Promise.try(function() {
+      return MediaPlugin.super_.prototype.processIncomeMessage.call(self, message);
+    })
+    .then(function(result) {
+      if ('trickle' == message['janus']) {
+        self._onTrickle(message);
+      }
+      return result;
+    });
+};
+
+MediaPlugin.prototype._onTrickle = function(incomeMessage) {
+  var candidate = new webrtcsupport.IceCandidate(incomeMessage['candidate']);
+  this._pc.addIceCandidate(candidate).catch(function(error) {
+    //TODO how to proceed?
+  });
+};
+
+module.exports = MediaPlugin;
