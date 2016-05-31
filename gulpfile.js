b07dbc995e95e47ee31fe258487f9d6f589e050c
vogdb <vozhdb@gmail.com>
2016-05-31 22:21:15 +0100

    Externalize vendor dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,20 @@ var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var gutil = require('gulp-util');
 var exorcist = require('exorcist');
+var nodeResolve = require('resolve');
+
+var vendor = [
+  'bluebird'
+];
 
 gulp.task('browserify', function() {
   var b = browserify({
     entries: './src/browser.js',
     debug: true
+  });
+
+  vendor.forEach(function(lib) {
+    b.external(lib);
   });
 
   return b.bundle()
@@ -28,8 +37,33 @@ gulp.task('browserify', function() {
     .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('vendor', function() {
+  var b = browserify();
+
+  vendor.forEach(function(id) {
+    b.require(nodeResolve.sync(id), {expose: id});
+  });
+
+  var stream = b
+    .bundle()
+    .on('error', function(err) {
+      console.log(err.message);
+      this.emit('end');
+    })
+    .pipe(source('vendor.js'));
+
+  stream
+    .pipe(gulp.dest('./dist'))
+    .pipe(rename('vendor.min.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist'));
+
+  return stream;
+});
+
 gulp.task('default', function() {
-  gulp.run('browserify');
+  gulp.run(['browserify', 'vendor']);
 
   gulp.watch("./src/*.js", function() {
     gulp.run('browserify');
