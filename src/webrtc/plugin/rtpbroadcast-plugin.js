741eb28dc63e1d3418e325389c3e8f27f3ce6fdd
vogdb <vozhdb@gmail.com>
2016-06-22 16:36:16 +0300

    RtpbroadcastPlugin through MediaStreamPlugin

--- src/webrtc/plugin/rtpbroadcast-plugin.js
+++ src/webrtc/plugin/rtpbroadcast-plugin.js
@@ -1,17 +1,13 @@
-var Promise = require('bluebird');
 var Helpers = require('../../helpers');
 var Plugin = require('../../plugin');
-var MediaEntityPlugin = require('../media-entity-plugin');
+var MediaStreamPlugin = require('../media-stream-plugin');
 
 function RtpbroadcastPlugin() {
   RtpbroadcastPlugin.super_.apply(this, arguments);
-
-  /** @type {number} */
-  this._currentMountpointId = null;
 }
 
 RtpbroadcastPlugin.NAME = 'janus.plugin.cm.rtpbroadcast';
-Helpers.inherits(RtpbroadcastPlugin, MediaEntityPlugin);
+Helpers.inherits(RtpbroadcastPlugin, MediaStreamPlugin);
 Plugin.register(RtpbroadcastPlugin.NAME, RtpbroadcastPlugin);
 
 
@@ -21,7 +17,7 @@ Plugin.register(RtpbroadcastPlugin.NAME, RtpbroadcastPlugin);
  */
 
 /**
- * @param {string} mountpointId
+ * @param {string} id
  * @param {Object} [options]
  * @param {string} [options.name]
  * @param {string} [options.description]
@@ -31,47 +27,33 @@ Plugin.register(RtpbroadcastPlugin.NAME, RtpbroadcastPlugin);
 
  * @return {Promise}
  */
-RtpbroadcastPlugin.prototype.create = function(mountpointId, options) {
-  return this._create(Helpers.extend({id: mountpointId}, options));
+RtpbroadcastPlugin.prototype.create = function(id, options) {
+  return this._create(id, options);
 };
 
 /**
- * @param {string} mountpointId
+ * @param {string} id
  * @return {Promise}
  */
-RtpbroadcastPlugin.prototype.destroy = function(mountpointId) {
-  return this._destroy({id: mountpointId})
-    .then(function() {
-      if (mountpointId == this._currentMountpointId) {
-        this._currentMountpointId = null;
-      }
-    }.bind(this));
+RtpbroadcastPlugin.prototype.destroy = function(id) {
+  return this._destroy(id);
 };
 
 /**
- * @param {string} [mountpointId]
+ * @param {string} [id]
  * @return {Promise}
  */
-RtpbroadcastPlugin.prototype.list = function(mountpointId) {
-  return this._list(mountpointId);
+RtpbroadcastPlugin.prototype.list = function(id) {
+  return this._list(id);
 };
 
 /**
- * @param {string} mountpointId
+ * @param {string} id
  * @param {Object} [answerOptions] {@link createAnswer}
  * @return {Promise}
  */
-RtpbroadcastPlugin.prototype.watch = function(mountpointId, answerOptions) {
-  var plugin = this;
-  return this.sendWithTransaction({body: {request: 'watch', id: mountpointId}})
-    .then(function(response) {
-      var jsep = response['jsep'];
-      if (!jsep || 'offer' != jsep['type']) {
-        throw new Error('Expect offer response on watch request')
-      }
-      plugin._currentMountpointId = mountpointId;
-      return plugin._startMediaStreaming(jsep, answerOptions).return(response);
-    });
+RtpbroadcastPlugin.prototype.watch = function(id, answerOptions) {
+  return this._watch(id, null, answerOptions);
 };
 
 /**
@@ -87,35 +69,29 @@ RtpbroadcastPlugin.prototype.watchUDP = function(mountpointId, streams) {
  * @return {Promise}
  */
 RtpbroadcastPlugin.prototype.start = function() {
-  return this.sendWithTransaction({body: {request: 'start'}});
+  return this._start();
 };
 
 /**
  * @return {Promise}
  */
 RtpbroadcastPlugin.prototype.stop = function() {
-  return this.sendWithTransaction({body: {request: 'stop'}})
-    .then(function() {
-      this._currentMountpointId = null;
-    }.bind(this));
+  return this._stop();
 };
 
 /**
  * @return {Promise}
  */
 RtpbroadcastPlugin.prototype.pause = function() {
-  return this.sendWithTransaction({body: {request: 'pause'}});
+  return this._pause();
 };
 
 /**
- * @param {string} mountpointId
+ * @param {string} id
  * @return {Promise}
  */
-RtpbroadcastPlugin.prototype.switch = function(mountpointId) {
-  return this.sendWithTransaction({body: {request: 'switch', id: mountpointId}})
-    .then(function() {
-      this._currentMountpointId = mountpointId;
-    }.bind(this));
+RtpbroadcastPlugin.prototype.switch = function(id) {
+  return this._switch(id);
 };
 
 /**
@@ -127,45 +103,11 @@ RtpbroadcastPlugin.prototype.switchSource = function(index) {
 };
 
 /**
- * @param {string} mountpointId
- * @param {Object} [answerOptions] {@link watch}
- * @return {Promise}
- */
-RtpbroadcastPlugin.prototype.connect = function(mountpointId, answerOptions) {
-  if (mountpointId == this._currentMountpointId) {
-    return Promise.resolve();
-  }
-  if (this._currentMountpointId) {
-    return this.switch(mountpointId);
-  }
-  return this.watch(mountpointId, answerOptions);
-};
-
-/**
  * @param {boolean} enabled
  * @return {Promise}
  */
 RtpbroadcastPlugin.prototype.superuser = function(enabled) {
   return this.sendWithTransaction({body: {request: 'superuser', enabled: enabled}});
-};
-
-/**
- * @param {RTCSessionDescription} jsep
- * @param {RTCAnswerOptions} [answerOptions]
- * @return {Promise}
- */
-RtpbroadcastPlugin.prototype._startMediaStreaming = function(jsep, answerOptions) {
-  var self = this;
-  return Promise
-    .try(function() {
-      self.createPeerConnection();
-    })
-    .then(function() {
-      return self.createAnswer(jsep, answerOptions);
-    })
-    .then(function(jsep) {
-      return self.send({janus: 'message', body: {request: 'start'}, jsep: jsep});
-    });
 };
 
 module.exports = RtpbroadcastPlugin;
